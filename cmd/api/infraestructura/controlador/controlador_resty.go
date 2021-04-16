ec423b855e885b203d11ac72406be28a371766ac
oscar.ruiz <oscar.ruiz@ceiba.com.co>
2021-04-16 09:46:07 -0500

    Ajustes peticiones resty para que retorne un json

--- cmd/api/infraestructura/controlador/controlador_resty.go
+++ cmd/api/infraestructura/controlador/controlador_resty.go
@@ -1,11 +1,41 @@
 package controlador
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/go-resty/resty/v2"
 	"net/http"
 )
+
+type usuario struct {
+	Id               int    `json:"id"`
+	Uid              string `json:"uid"`
+	Nombre           string `json:"nombre"`
+	Email            string `json:"email"`
+	Foto             string `json:"foto"`
+	Fecha_nacimiento string `json:"fecha_nacimiento"`
+	Telefono         string `json:"telefono"`
+	Genero           string `json:"genero"`
+	Departamento     string `json:"departamento"`
+	Municipio        string `json:"municipio"`
+}
+
+type producto struct {
+	Id          int    `json:"id"`
+	Nombre      string `json:"nombre"`
+	Descripcion string `json:"descripcion"`
+	Precio      string `json:"precio"`
+	Foto        string `json:"foto"`
+	Lugar       int    `json:"lugar"`
+}
+
+type pelicula struct {
+	Page          int         `json:"page"`
+	Results       interface{} `json:"results"`
+	Total_pages   int         `json:"total_pages"`
+	Total_results int         `json:"total_results"`
+}
 
 func GetPeliculasResty(context *gin.Context) {
 	client := resty.New()
@@ -26,7 +56,11 @@ func GetPeliculasResty(context *gin.Context) {
 	fmt.Println("  Received At:", resp.ReceivedAt())
 	fmt.Println("  Body       :\n", resp)
 
-	context.String(http.StatusOK, resp.String())
+	var peliculas []pelicula
+	bytes := []byte("[" + resp.String() + "]")
+	_ = json.Unmarshal(bytes, &peliculas)
+	fmt.Println("La data", peliculas)
+	context.JSON(http.StatusOK, peliculas)
 }
 
 func GetProductosResty(context *gin.Context) {
@@ -45,7 +79,10 @@ func GetProductosResty(context *gin.Context) {
 	fmt.Println("  Received At:", resp.ReceivedAt())
 	fmt.Println("  Body       :\n", resp)
 
-	context.String(http.StatusOK, resp.String())
+	var productos []producto
+	bytes := []byte(resp.String())
+	_ = json.Unmarshal(bytes, &productos)
+	context.JSON(http.StatusOK, productos)
 }
 
 func GetUsuariosResty(context *gin.Context) {
@@ -64,7 +101,10 @@ func GetUsuariosResty(context *gin.Context) {
 	fmt.Println("  Received At:", resp.ReceivedAt())
 	fmt.Println("  Body       :\n", resp)
 
-	context.String(http.StatusOK, resp.String())
+	var usuarios []usuario
+	bytes := []byte(resp.String())
+	_ = json.Unmarshal(bytes, &usuarios)
+	context.JSON(http.StatusOK, usuarios)
 }
 
 func PostUsuariosResty(context *gin.Context) {
